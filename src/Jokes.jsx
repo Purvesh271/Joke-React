aa18001b6ccfa6a451fc22081d63bdf4fb9bf7f7
Purvesh271 <ppurvesh923@gmail.com>
2024-02-24 00:34:57 +0530

    added useEffect Hook for getting automatic joke on first render

--- src/Jokes.jsx
+++ src/Jokes.jsx
@@ -1,21 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 
 export default function Jokes(){
 
     let[Joke,setJoke] = useState({});
+    
+    const url = "https://official-joke-api.appspot.com/random_joke";
 
+    //API CALL FUNCTION
     const newJoke = async ()=>{
         let res = await fetch(url);
         let jsonRes = await res.json();
         console.log(jsonRes);
         setJoke({setup: jsonRes.setup, punchline: jsonRes.punchline});
         
-    }
-    const url = "https://official-joke-api.appspot.com/random_joke";
-   
+    };
 
 
+    //useEffect for getting a joke at first render by default
+    useEffect( () =>{
+        async function firstJoke(){
+            let res = await fetch(url);
+            let jsonRes = await res.json();
+            console.log(jsonRes);
+            setJoke({setup: jsonRes.setup, punchline: jsonRes.punchline});    
+        }
+        firstJoke();
+    }, [] ) //empty array in dependency means useEffect works only at the first render
+
+
+
+    //Main Ui
     return(
         <div>
             <h1>Funny Jokes!!</h1>
